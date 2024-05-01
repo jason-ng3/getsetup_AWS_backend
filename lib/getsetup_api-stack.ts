632c417b8a6a297108ad43f7fc23865738dbac28
Jason Ng <jasonscoat@yahoo.com>
2024-05-01 14:14:25 -0400

    Fix 502 API Gateway error

--- lib/getsetup_api-stack.ts
+++ lib/getsetup_api-stack.ts
@@ -3,14 +3,14 @@ import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigateway from '@aws-cdk/aws-apigateway';
 import * as path from 'path';
 
-const assetPath = path.join(__dirname, 'lambdas', 'multiply');
+const assetPath = path.join(__dirname, './lambdas/multiply');
 
 export class GetsetupApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const multiplyFunction = new lambda.Function(this, 'MultiplyFunction', {
-      runtime: lambda.Runtime.NODEJS_14_X,
+      runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(assetPath)
     });
--- lib/lambdas/multiply/index.ts
+++ lib/lambdas/multiply/index.ts
@@ -1,12 +1,33 @@
-interface MathOperationEvent {
-  body: {
-    num1: number;
-    num2: number;
-  };
-}
+type APIGatewayProxyResult = {
+  statusCode: number;
+  headers: { [key: string]: string };
+  body: string;
+};
 
-export async function handler(event: MathOperationEvent): Promise<{ result: number }> {
-  const { num1, num2 } = event.body;
+export async function handler(event: any): Promise<APIGatewayProxyResult> {
+  if (!event.body) {
+    throw new Error('Request data is missing');
+  }
+
+  let parsedBody;
+  try {
+    parsedBody = JSON.parse(event.body);
+
+    if (typeof parsedBody.num1 !== 'number' || typeof parsedBody.num2 !== 'number') {
+      throw new Error('Invalid request data: num1 and num2 must be numbers');
+    } 
+  } catch (error: any) {
+    throw new Error('Error parsing request body as JSON: ' + error.message);
+  }
+
+  const { num1, num2 } = parsedBody;
   const result = num1 * num2;
-  return { result };
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ result })
+  };
 }
